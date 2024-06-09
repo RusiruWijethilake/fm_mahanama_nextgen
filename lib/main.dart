d19b58242ee7472a9c43bc5e7cab97966611d034
Rusiru Wijethilake <rusirumc@gmail.com>
2024-06-09 12:20:40 +0530

    Added Firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
+import 'dart:ui';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fm_mahanama_nextgen/constants/app_fonts_theme.dart';
 import 'package:fm_mahanama_nextgen/constants/app_theme.dart';
+import 'package:fm_mahanama_nextgen/firebase_options.dart';
 import 'package:fm_mahanama_nextgen/providers/theme_provider/theme_provider.dart';
 import 'package:fm_mahanama_nextgen/ui/home_page.dart';
 import 'package:fm_mahanama_nextgen/utils/routes/routes.dart';
@@ -12,12 +17,27 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setPreferredOrientations();
+  initFirebaseServices();
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
       child: const MyApp(),
     ),
   );
+}
+
+Future<void> initFirebaseServices() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
 }
 
 Future<void> setPreferredOrientations() {
